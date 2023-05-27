69a3e6063198061b51de3434eea185dbb47d574f
anitazgb <anitazgb1@gmail.com>
2023-05-27 03:52:19 -0700

    pushing working version of the form

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function validateForm() {
   var errorMessages = ""; // Variable to store error messages
 
+  // Variables control the status of each field and initially set to false
+  var validFirstname = false;
+  var validLastname = false;
+  var validEmail = false;
+  var validPhone = false;
+  var validUsername = false;
+  var validPassword = false;
+  var validAddress = false;
+  var validCity = false;
+  var validState = false;
+  var validCountry = false;
+  var validZipcode = false;
+
   // Get form field values
   var firstname = document.getElementById("firstname").value;
   var lastname = document.getElementById("lastname").value;
@@ -15,58 +28,96 @@ function validateForm() {
   var zipcode = document.getElementById("zipcode").value;
 
   // Validate FirstName
-  if (firstname === "" || firstname.length > 20 || !/^[A-Za-z]+$/.test(firstname)) {
+  if (firstname === "" || firstname === "null" || firstname.length > 20 || !/^[A-Za-z]+$/.test(firstname)) {
     errorMessages += "<p>The firstname is required and should contain only alphabetical characters, maximum 20 characters.</p>";
+  } else {
+    validFirstname = true;
   }
 
   // Validate LastName
-  if (lastname === "" || lastname.length > 50 || !/^[A-Za-z]+$/.test(lastname)) {
+  if (lastname === "" || lastname === "null" || lastname.length > 50 || !/^[A-Za-z]+$/.test(lastname)) {
     errorMessages += "<p>The lastname is required and should contain only alphabetical characters, maximum 50 characters.</p>";
+  } else {
+    validLastname = true;
   }
 
   // Validate Email
-  if (email === "" || !/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email)) {
+  var atpos = email.indexOf("@");
+  var dotpos = email.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
     errorMessages += "<p>Invalid email address.</p>";
+  } else {
+    validEmail = true;
   }
 
   // Validate Phone
-  if (phone === "" || !/^[0-9]+$/.test(phone) || phone.length > 15) {
-    errorMessages += "<p>Invalid phone number. Only numerical values are allowed, maximum 15 digits.</p>";
+  var formattedPhone = phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+  if (isNaN(phone) || phone.length > 15 || phone === null || phone === "") {
+    errorMessages = "<p>Invalid phone number</p>";
+  } else {
+    document.getElementById("phone").value = formattedPhone;
+    validPhone = true;
   }
 
   // Validate Username
-  if (username === "" || username.length > 12) {
+  if (username === "" || username === "null" || username.length > 12) {
     errorMessages += "<p>Username is required and should be maximum 12 characters.</p>";
+  } else {
+    validUsername = true;
   }
 
   // Validate Password
-  if (password === "" || password.length > 7) {
-    errorMessages += "<p>Password is required and should be maximum 7 characters.</p>";
+  var uppercase = /[A-Z]/;
+  var lowercase = /[a-z]/;
+  var number = /[0-9]/;
+  var specialChar = /[!@#$%^&*]/;
+
+  if (
+    password === "" ||
+    password.length > 7 ||
+    !uppercase.test(password) ||
+    !lowercase.test(password) ||
+    !number.test(password) ||
+    !specialChar.test(password)
+  ) {
+    errorMessages += "<p>Password is required and should contain 1 UPPER, 1 lower, 1 number, and 1 special character.</p>";
+  } else {
+    validPassword = true;
   }
 
   // Validate Address
-  if (address === "") {
+  if (address === "" || address === "null") {
     errorMessages += "<p>Address is required.</p>";
+  } else {
+    validAddress = true;
   }
 
   // Validate City
-  if (city === "") {
+  if (city === "" || city === "null") {
     errorMessages += "<p>City is required.</p>";
+  } else {
+    validCity = true;
   }
 
   // Validate State
-  if (state === "") {
+  if (state === "" || state === null) {
     errorMessages += "<p>State is required.</p>";
+  } else {
+    validState = true;
   }
 
   // Validate Country
-  if (country === "") {
+  if (country === "" || country === null) {
     errorMessages += "<p>Country is required.</p>";
+  } else {
+    validCountry = true;
   }
 
   // Validate ZipCode
-  if (country === "USA" && (zipcode === "" || !/^[0-9]{5}$/.test(zipcode))) {
+  if (country === "USA" && (zipcode === "" || zipcode === null || !/^[0-9]{5}$/.test(zipcode))) {
     errorMessages += "<p>Invalid ZIP code. Required only if the country is USA, and should be 5 digits.</p>";
+  } else {
+    validZipcode = true;
   }
 
   // Display error messages
